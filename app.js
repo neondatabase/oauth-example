1391aa28aaa8dd77cb9dbb00a7a8ce6a60fb0619
Eduard Dyckman <bird.duskpoet@gmail.com>
2022-08-30 19:55:57 +0300

    work on project create

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ const listen_port = process.env.PORT || 5555
 
 const neon_oauth_url = 'https://oauth2.stage.neon.tech'
 const neon_api_url = 'https://console.stage.neon.tech/api/v1'
+let neonIssuer = await Issuer.discover(neon_oauth_url)
+// Store the code_verifier in memory
+const state = generators.state()
+const codeVerifier = generators.codeVerifier()
+const codeChallenge = generators.codeChallenge(codeVerifier)
+
+const getProtocol = (host) => (host.startsWith('localhost') || host.startsWith('127.0.0.1')) ? 'http' : 'https'
 
 // Show index page with link to Neon project creation
 app.get('/', async (req, res) => {
   // Instantiate OAuth client
   console.log('GOT REQUEST', req.url)
-  const redirect_uri = (req.get('host').startsWith('localhost') ? 'http' : 'https') + '://' + req.get('host') + '/callback'
+  const redirect_uri = getProtocol(req.get('host')) + '://' + req.get('host') + '/callback'
   console.log('REDIRECT URL IS:', redirect_uri);
 
-  let neonIssuer = await Issuer.discover(neon_oauth_url)
   const neonOAuthClient = new neonIssuer.Client({
     client_id: process.env.NEON_OAUTH_ID,
     redirect_uris: [redirect_uri],
@@ -24,13 +30,8 @@ app.get('/', async (req, res) => {
     client_secret: process.env.NEON_OAUTH_SECRET,
   })
 
-  // Store the code_verifier in memory
-  const state = generators.state()
-  const codeVerifier = generators.codeVerifier()
-  const codeChallenge = generators.codeChallenge(codeVerifier)
-
   const authUrl = neonOAuthClient.authorizationUrl({
-    scope: `openid offline`,
+    scope: `openid offline urn:neoncloud:projects:create`,
     state,
     code_challenge: codeChallenge,
     code_challenge_method: 'S256',
@@ -42,9 +43,16 @@ app.get('/', async (req, res) => {
 app.get('/callback', async (req, res) => {
   console.log('GOT CB REQUEST', req.url)
 
-  const redirect_uri = (req.get('host').startsWith('localhost') ? 'http' : 'https') + '://' + req.get('host') + '/callback'
+  const redirect_uri = getProtocol(req.get('host')) + '://' + req.get('host') + '/callback'
   console.log('REDIRECT URL IS:', redirect_uri);
   try {
+    const neonOAuthClient = new neonIssuer.Client({
+      client_id: process.env.NEON_OAUTH_ID,
+      redirect_uris: [redirect_uri],
+      response_types: ['code'],
+      client_secret: process.env.NEON_OAUTH_SECRET,
+    })
+
     // finish OAuth and get access_token
     const params = neonOAuthClient.callbackParams(req);
     const tokenSet = await neonOAuthClient.callback(redirect_uri, params, { code_verifier: codeVerifier, state });
@@ -57,17 +65,12 @@ app.get('/callback', async (req, res) => {
 
     // We need to get project that we have access to. Normally it would one,
     // so select the first.
-    let project = (await neonClient.get(`/projects`)).data[0];
+    const project = (await neonClient.post('/projects', {project: {settings: {}}})).data;
+    console.log('SUCCESS! Project is:', project)
 
-    // Use the first user and database in this project. "web_access" is an internal role
-    // and will disapper soon, so filter it out.
-    let role = project.roles.filter(role => role.name != "web_access")[0]
-    let dbname = project.databases[0].name
+    const dsn_with_pass = project.roles.filter((role) => role.name !== 'web_access')[0].dsn
 
-    // Neon does not store role passwords, so we need to reset it to get an unencrypted one.
-    let dsn_with_pass = (await neonClient.post(`/projects/${project.id}/roles/${role.name}/reset_password`)).data.dsn;
-
-    console.log('SUCCESS! DSN is:', dsn_with_pass)
+    const dbname = project.databases[0].name
     // Okay, we can show connection string
     res.send(`Done: you can connect to '${dsn_with_pass}/${dbname}'`)
   } catch (e) {
